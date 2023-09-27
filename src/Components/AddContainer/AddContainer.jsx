f5376bfb04d5c7d55c46008fd781df2e67c1eac6
Agil Bayramli <aqilbayramli96@gmail.com>
2023-09-27 17:34:35 +0400

    V1.3

--- src/Components/AddContainer/AddContainer.jsx
+++ src/Components/AddContainer/AddContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./AddContainer.css"
 
@@ -8,20 +8,33 @@ function AddContainer() {
     const [name, setName] = useState("");
     const [surname, setSurname] = useState("");
     const [number, setNumber] = useState("");
-    const [data, setData] = useState([])
+    const [data, setData] = useState(JSON.parse(localStorage.getItem('localData')));
+    const newData = {
+        name,
+        surname,
+        number
+    }
 
     function saveToDo(e) {
         e.preventDefault();
-        setData(prev => ([...prev,{
-            name:name,
-            surname:surname,
-            number:number
-        }]))
+        setData(prev => ([...prev,newData]));
         setName("");
         setSurname("");
         setNumber("");
-
     }
+
+    useEffect(() => {
+        localStorage.setItem("localData",JSON.stringify(data));
+    },[data])
+
+    const deleteData = (index)=>{
+        let deleted = [...data];
+        deleted = deleted.filter((_,i) => i !== index)
+
+        localStorage.setItem('localData',JSON.stringify(deleted));
+        setData(deleted);
+    }
+
     return (
         <div>
             <div className='add__container'>
@@ -34,7 +47,7 @@ function AddContainer() {
 
                 <div className="mb-3 col-6">
                     <label htmlFor="inputSurname" className="form-label">Surname</label>
-                    <input type="text" className="form-control" id="inputSurname" required value={surname} onChange={e => setSurname(e.target.value)}>
+                    <input type="text" className="form-control sadsadsada" id="inputSurname" required value={surname} onChange={e => setSurname(e.target.value)}>
                     </input>
                 </div>
 
@@ -67,7 +80,7 @@ function AddContainer() {
                                 </td>
                                 <td>{item.surname}</td>
                                 <td>{item.number}</td>
-                                <td><i class="fa-solid fa-trash delete__icon"></i></td>
+                                <td><i onClick={() => deleteData(index)} class="fa-solid fa-trash delete__icon"></i></td>
                             </tr>
                             ))
                         }
@@ -77,11 +90,6 @@ function AddContainer() {
         </div>
     )
 }
-
-
-  
-
-
 
 export default AddContainer
 
